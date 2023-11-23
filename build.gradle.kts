e7d5529862e3d6b744aabae48e7805b965a5d2ee
原俊杰 <yuan@junjie.pro>
2023-11-24 01:29:33 +0800

    升级到Java 21。
    Signed-off-by: 原俊杰 <yuan@junjie.pro>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     java
-    id("org.springframework.boot") version "3.1.3"
-    id("io.spring.dependency-management") version "1.1.3"
-    id("org.hibernate.orm") version "6.2.7.Final"
-    id("org.graalvm.buildtools.native") version "0.9.24"
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.hibernate.orm") version "6.3.1.Final"
+    id("org.graalvm.buildtools.native") version "0.9.28"
 }
 
 group = "pro.junjie"
 version = "0.1.0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
